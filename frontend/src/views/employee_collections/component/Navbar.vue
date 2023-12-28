bea9e49527525cef0f7667f16244627538c7a47c
Huakhanhdoan <21020759@vnu.edu.vn>
2023-12-28 16:52:26 +0700

    them employee collection

--- frontend/src/views/employee_collections/component/Navbar.vue
+++ frontend/src/views/employee_collections/component/Navbar.vue
@@ -8,17 +8,12 @@
     <nav class="dashboard-nav-list">
       <router-link to="/employee_collection">
         <a href="#" class="dashboard-nav-item"
-          ><i class="fas fa-box-open"></i> Đơn hàng từ điểm tập kết
+          ><i class="fas fa-box-open"></i> Xử lý đơn hàng
         </a>
       </router-link>
-      <router-link to="/employee_collection"
-        ><a href="#" class="dashboard-nav-item"
-          ><i class="fas fa-cart-plus"></i> Đơn hàng từ điểm giao dịch
-        </a></router-link
-      >
 
       <div class="nav-item-divider"></div>
-      <router-link to="" @click="logout">
+      <router-link to="/" @click="logout">
         <a href="#" class="dashboard-nav-item"
           ><i class="fas fa-sign-out-alt"></i> Đăng xuất</a
         ></router-link
