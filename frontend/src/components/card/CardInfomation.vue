8246a44b67ec599308a30feed9d483a1848cf499
Huakhanhdoan <21020759@vnu.edu.vn>
2023-12-29 20:42:58 +0700

    add search id

---
+++ frontend/src/components/card/CardInfomation.vue
@@ -0,0 +1,124 @@
+<template>
+  <v-card style="background-color: cornsilk">
+    <v-card-title>Thông tin đơn hàng</v-card-title>
+    <v-card-text>
+      <v-row>
+        <v-col cols="6" style="display: flex">
+          <v-icon id="icon" size="20" class="me-2">
+            <i class="fas fa-address-card"></i>
+          </v-icon>
+          <p style="font-weight: bold">ID:</p>
+          <p>{{ idOder }}</p>
+        </v-col>
+        <v-col cols="6" style="display: flex">
+          <v-icon id="icon" size="20" class="me-2">
+            <i class="far fa-calendar-alt"></i>
+          </v-icon>
+          <p style="font-weight: bold">Ngày gửi:</p>
+          <p>{{ date }}</p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" style="display: flex">
+          <v-icon id="icon" size="20" class="me-2">
+            <i class="fas fa-business-time"></i>
+          </v-icon>
+          <p style="font-weight: bold">Trạng thái:</p>
+          <p>Đơn hàng đang ở điểm tập kết Cầu Giấy</p>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="6">
+          <v-card style="background-color: cornsilk">
+            <v-card-title>Thông tin người gửi</v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col cols="12" style="display: flex">
+                  <v-icon id="icon" size="20" class="me-2">
+                    <i class="fas fa-address-book"></i>
+                  </v-icon>
+                  <p style="font-weight: bold">Họ tên:</p>
+                  <p>{{ sender.name }}</p>
+                </v-col>
+                <v-col cols="12" style="display: flex">
+                  <v-icon id="icon" size="20" class="me-2">
+                    <i class="fas fa-phone-square"></i>
+                  </v-icon>
+                  <p style="font-weight: bold">Số điện thoại:</p>
+                  <p>{{ sender.phone }}</p>
+                </v-col>
+                <v-col cols="12" style="display: flex">
+                  <v-icon id="icon" size="20" class="me-2">
+                    <i class="fas fa-building"></i>
+                  </v-icon>
+                  <p style="font-weight: bold">Địa chỉ:</p>
+                  <p>{{ sender.address }}</p>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="6">
+          <v-card style="background-color: cornsilk">
+            <v-card-title>Thông tin người nhận</v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col cols="12" style="display: flex">
+                  <v-icon id="icon" size="20" class="me-2">
+                    <i class="far fa-address-book"></i>
+                  </v-icon>
+                  <p style="font-weight: bold">Họ tên:</p>
+                  <p>{{ receiver.name }}</p>
+                </v-col>
+                <v-col cols="12" style="display: flex">
+                  <v-icon id="icon" size="20" class="me-2">
+                    <i class="fas fa-phone"></i>
+                  </v-icon>
+                  <p style="font-weight: bold">Số điện thoại:</p>
+                  <p>{{ receiver.phone }}</p>
+                </v-col>
+                <v-col cols="12" style="display: flex">
+                  <v-icon id="icon" size="20" class="me-2">
+                    <i class="far fa-building"></i>
+                  </v-icon>
+                  <p style="font-weight: bold">Địa chỉ:</p>
+                  <p>{{ receiver.address }}</p>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    idOder: String,
+  },
+  data() {
+    return {
+      sender: {
+        name: "",
+        address: "",
+        phone: "",
+      },
+      receiver: {
+        name: "",
+        address: "",
+        phone: "",
+      },
+      order: {
+        weight: "",
+        name: "",
+        type: "",
+      },
+      date: "",
+    };
+  },
+};
+</script>
+
+<style></style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,98 +1,103 @@
 /* eslint-disable */
 <template>
-  <!-- Menu -->
-  <MenuVue :links="links">Magic<span class="prim">Post</span></MenuVue>
-  <PageSection id="page" pic="header.png">
-    <template v-slot:welcome>Tra cứu đơn hàng</template>
-    <form>
-      <div class="form-group" id="input">
-        <input
-          type="input"
-          class="form-control"
-          id="exampleInput"
-          placeholder="Nhập mã vận đơn của bạn."
-        />
-      </div>
-      <button type="submit" class="btn btn-primary" style="color: white">
-        Xác nhận
-      </button>
-    </form>
-  </PageSection>
-  <HeroSection id="hero" pic="UAV.png">
-    <template v-slot:welcome>Về chúng tôi</template>
-    <b>MagicPost</b>
-    <hi-word>
-      là thương hiệu chuyển phát nhanh dựa trên sự phát triển của công nghệ và
-      Internet. Chúng tôi sở hữu mạng lưới rộng khắp nhằm hỗ trợ các hoạt động
-      giao nhận hàng hóa nhanh chóng không chỉ ở nội thành mà còn ở ngoại thành
-      và các vùng xa của các tỉnh thành trong cả nước Việt Nam.
-    </hi-word>
-  </HeroSection>
-  <SkillsGrid id="skills" :skills="skills" />
-  <ExperienceTable id="edu-exp" :experience="experience" />
-  <AwardsGrid id="awards" :awards="awards" />
-  <ProjectsGrid id="projects" :projects="projects" />
-  <div class="bg-dark text-white">
-    <div class="d-flex justify-content-around">
-      <div>
-        <br />
-        Magic<span class="prim">Post</span>
-        <h5>CÔNG TY TNHH MỘT THÀNH VIÊN CHUYỂN PHÁT NHANH</h5>
-        <div class="h7">
-          Giấy CNĐKKD: 0313617136 - Ngày cấp: 13/01/2016, đăng ký thay đổi lần
-          06 ngày 25/09/2018 <br />
-          Cơ Quan Cấp: Phòng Đăng ký kinh doanh - Sở kế hoạch và đầu tư TP Hồ
-          Chí Minh
-        </div>
-      </div>
-      <div>
-        <br /><br />
-        <h5>THÔNG TIN LIÊN HỆ</h5>
-        <div class="h7">
-          <ul class="nav flex-column">
-            <li class="nav-item mb2">E-Mail: cskh@jtexpress.vn</li>
-            <li class="nav-item mb2">Phone: 1900 1088</li>
-            <li class="nav-item mb2">
-              Address: 10 Mai Chí Thọ, P. Thủ Thiêm, Thành phố Thủ Đức, TP. HCM
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div>
-        <br /><br />
-        <h5>TẢI ỨNG DỤNG</h5>
-        <div class="d-flex flex-row">
-          <img
-            src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=https://github.com/Huakhanhdoan/magic_post/"
-            title="Link to Download"
-            class="qr-code"
-            alt="Giao hàng Chuyển phát nhanh"
+  <div>
+    <!-- Menu -->
+    <MenuVue :links="links">Magic<span class="prim">Post</span></MenuVue>
+    <PageSection id="page" pic="header.png">
+      <template v-slot:welcome>Tra cứu đơn hàng</template>
+      <form>
+        <div class="form-group" id="input">
+          <input
+            v-model="idOrder"
+            type="input"
+            class="form-control"
+            id="exampleInput"
+            placeholder="Nhập mã vận đơn của bạn."
           />
-          <div class="ml-5 d-flex flex-column">
-            <a
-              href="https://apps.apple.com/us/app/j-t-vnm-vip/id1474072185"
-              target="_blank"
-            >
-              <img
-                src="https://jtexpress.vn/storage/app/uploads/public/627/5ca/259/6275ca259876b348340439.png"
-                alt="J&T Express - Giao hàng Chuyển phát nhanh"
-              />
-            </a>
-            <a
-              href="https://play.google.com/store/apps/details?id=com.msd.VIPyueNanClient&amp;hl=vi&amp;gl=US"
-              target="_blank"
-            >
-              <img
-                src="https://jtexpress.vn/storage/app/uploads/public/627/5ca/bd7/6275cabd7222d202962202.png"
-                alt="J&T Express - Giao hàng Chuyển phát nhanh"
-              />
-            </a>
+        </div>
+        <v-btn @click="search" color="primary">Xác nhận</v-btn>
+      </form>
+      <div v-if="dialog" max-width="600px" style="margin-top: 20px">
+        <card-infomation :idOder="idOrder"></card-infomation>
+      </div>
+    </PageSection>
+    <HeroSection id="hero" pic="UAV.png">
+      <template v-slot:welcome>Về chúng tôi</template>
+      <b>MagicPost</b>
+      <hi-word>
+        là thương hiệu chuyển phát nhanh dựa trên sự phát triển của công nghệ và
+        Internet. Chúng tôi sở hữu mạng lưới rộng khắp nhằm hỗ trợ các hoạt động
+        giao nhận hàng hóa nhanh chóng không chỉ ở nội thành mà còn ở ngoại
+        thành và các vùng xa của các tỉnh thành trong cả nước Việt Nam.
+      </hi-word>
+    </HeroSection>
+    <SkillsGrid id="skills" :skills="skills" />
+    <ExperienceTable id="edu-exp" :experience="experience" />
+    <AwardsGrid id="awards" :awards="awards" />
+    <ProjectsGrid id="projects" :projects="projects" />
+    <div class="bg-dark text-white">
+      <div class="d-flex justify-content-around">
+        <div>
+          <br />
+          Magic<span class="prim">Post</span>
+          <h5>CÔNG TY TNHH MỘT THÀNH VIÊN CHUYỂN PHÁT NHANH</h5>
+          <div class="h7">
+            Giấy CNĐKKD: 0313617136 - Ngày cấp: 13/01/2016, đăng ký thay đổi lần
+            06 ngày 25/09/2018 <br />
+            Cơ Quan Cấp: Phòng Đăng ký kinh doanh - Sở kế hoạch và đầu tư TP Hồ
+            Chí Minh
+          </div>
+        </div>
+        <div>
+          <br /><br />
+          <h5>THÔNG TIN LIÊN HỆ</h5>
+          <div class="h7">
+            <ul class="nav flex-column">
+              <li class="nav-item mb2">E-Mail: cskh@jtexpress.vn</li>
+              <li class="nav-item mb2">Phone: 1900 1088</li>
+              <li class="nav-item mb2">
+                Address: 10 Mai Chí Thọ, P. Thủ Thiêm, Thành phố Thủ Đức, TP.
+                HCM
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <br /><br />
+          <h5>TẢI ỨNG DỤNG</h5>
+          <div class="d-flex flex-row">
+            <img
+              src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=https://github.com/Huakhanhdoan/magic_post/"
+              title="Link to Download"
+              class="qr-code"
+              alt="Giao hàng Chuyển phát nhanh"
+            />
+            <div class="ml-5 d-flex flex-column">
+              <a
+                href="https://apps.apple.com/us/app/j-t-vnm-vip/id1474072185"
+                target="_blank"
+              >
+                <img
+                  src="https://jtexpress.vn/storage/app/uploads/public/627/5ca/259/6275ca259876b348340439.png"
+                  alt="J&T Express - Giao hàng Chuyển phát nhanh"
+                />
+              </a>
+              <a
+                href="https://play.google.com/store/apps/details?id=com.msd.VIPyueNanClient&amp;hl=vi&amp;gl=US"
+                target="_blank"
+              >
+                <img
+                  src="https://jtexpress.vn/storage/app/uploads/public/627/5ca/bd7/6275cabd7222d202962202.png"
+                  alt="J&T Express - Giao hàng Chuyển phát nhanh"
+                />
+              </a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="text-center">
-      <FooterVue :links="socials" :copyright="copyright" />
+      <div class="text-center">
+        <FooterVue :links="socials" :copyright="copyright" />
+      </div>
     </div>
   </div>
 </template>
@@ -112,11 +117,14 @@ import skills from "@/data/skills";
 import experience from "@/data/experience";
 import awards from "@/data/awards";
 import projects from "@/data/projects";
+import CardInfomation from "../components/card/CardInfomation.vue";
 
 export default {
   name: "HomeView",
   data: () => {
     return {
+      dialog: false,
+      idOrder: "",
       delay: 100,
       links: [
         {
@@ -163,6 +171,13 @@ export default {
     ExperienceTable,
     AwardsGrid,
     ProjectsGrid,
+    CardInfomation,
+  },
+  methods: {
+    search() {
+      this.dialog = true;
+      console.log(this.idOder);
+    },
   },
 };
 </script>
